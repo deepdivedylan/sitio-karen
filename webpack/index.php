96d43f27c963234e420205a5237bb40f5f4cc55d
Dylan McDonald <dmcdonald21@cnm.edu>
2019-07-14 15:17:09 -0700

    added starter templates for HTML and CSS

---
+++ webpack/index.php
@@ -0,0 +1,14 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Because Fuzzy</title>
+	</head>
+	<body>
+		<h1>Because Fuzzy</h1>
+	</body>
+</html>
