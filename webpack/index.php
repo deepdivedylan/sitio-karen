33e815155fb4dc7449a9bcc44c18a125a1719a89
Dylan McDonald <dylan@deepdivedylan.io>
2019-09-04 19:19:35 -0700

    added translation to template

--- webpack/index.php
+++ webpack/index.php
@@ -1,14 +1,26 @@
 <?php
+require_once(dirname(__DIR__) . "/php/intl/Translator.php");
+
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
+
+if (empty($_SESSION["locale"]) === true) {
+    $locale = Translator::getLocale();
+    $_SESSION["locale"] = $locale;
+    setcookie("locale", $locale, time() + 2592000); // 30 day cookie
+} else {
+    $locale = $_SESSION["locale"];
+}
+$translator = new Translator($locale);
+
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo $locale;?>">
 	<head>
-		<title>Because Fuzzy</title>
+		<title><?php echo $translator->getTranslatedString("hi"); ?></title>
 	</head>
 	<body>
-		<h1>Because Fuzzy</h1>
+        <h1><?php echo $translator->getTranslatedString("hi"); ?></h1>
 	</body>
 </html>
