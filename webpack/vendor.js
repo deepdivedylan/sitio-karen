3de8b760abf68c6c205e06c7e056640a7f50dcf2
Dylan McDonald <dylan@deepdivedylan.io>
2019-09-11 20:21:14 -0700

    manually curating icons like this brought load time down by more than 1 MB :heart_eyes_cat: :joy_cat:

--- webpack/vendor.js
+++ webpack/vendor.js
@@ -1,4 +1,12 @@
 import 'popper.js';
 import 'jquery';
 import 'bootstrap';
-import '@fortawesome/fontawesome-free';
+
+// loading FontAwesome like this reduces page loads by 1 MB :scream_cat:
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import { faBalanceScale, faBars, faHandshake, faPassport } from '@fortawesome/free-solid-svg-icons';
+library.add(faBalanceScale);
+library.add(faBars);
+library.add(faHandshake);
+library.add(faPassport);
+dom.watch();
