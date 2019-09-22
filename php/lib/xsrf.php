93382e5b6be17c5f589e3feff612028e29d25851
Dylan McDonald <dmcdonald21@cnm.edu>
2019-09-21 17:17:36 -0700

    switched polyfills for XSRF headers

--- php/lib/xsrf.php
+++ php/lib/xsrf.php
@@ -1,36 +1,5 @@
 <?php
 /**
- * this if block exists because apache_request_headers() is not portable across web servers
- * this will clone apache_request_headers()'s functionality if the web server doesn't support apache_request_headers()
- * (e.g., non Apache web servers, Apache web servers with apache_request_headers() disabled)
- *
- * @see http://php.net/manual/en/function.apache-request-headers.php apache_request_headers()
- **/
-if(function_exists("apache_request_headers") === false) {
-	/**
-	 * clones apache_request_headers()'s behavior
-	 *
-	 * @return array all HTTP request headers
-	 **/
-	function apache_request_headers() {
-		$headers = array();
-		foreach($_SERVER as $header => $value) {
-			// divide the header name by the underbar
-			$headerNameArray = explode("_" , $header);
-			// request headers always are prefixed by HTTP_
-			if(array_shift($headerNameArray) === "HTTP") {
-				// convert HTTP_FOO_HEADER to Foo-Header
-				array_walk($headerNameArray, function(&$headerName) {
-					$headerName = ucfirst(strtolower($headerName));
-				});
-				$headers[join("-", $headerNameArray)] = $value;
-			}
-		}
-		return($headers);
-	}
-}
-
-/**
  * sets an XSRF cookie, generating one if necessary
  *
  * @param string $cookiePath path the cookie is relevant to, root by default
@@ -65,7 +34,7 @@ function verifyXsrf() {
 	}
 
 	// grab the XSRF token sent by Angular, jQuery, or JavaScript in the header
-	$headers = array_change_key_case(apache_request_headers(), CASE_UPPER);
+	$headers = array_change_key_case(getallheaders(), CASE_UPPER);
 	if(array_key_exists("X-XSRF-TOKEN", $headers) === false) {
 		throw(new InvalidArgumentException("invalid XSRF token", 401));
 	}
